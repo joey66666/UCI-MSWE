b3bc20f2c37c4943062d8876860668799bd5fce3
joey66666 <zzjoeyyy@foxmail.com>
2020-12-08 16:53:11 +0800

    clean repo

--- 250P/Practice M6/MailBag/client/src/code/config.js
+++ 250P/Practice M6/MailBag/client/src/code/config.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 exports.config = {
     serverAddress: "http://localhost",
-    userEmail: "joey66666@aliyun.com"
+    userEmail: ""
 };
 //# sourceMappingURL=config.js.map
--- 250P/Practice M6/MailBag/client/src/code/config.ts
+++ 250P/Practice M6/MailBag/client/src/code/config.ts
@@ -1,4 +1,4 @@
 export const config: { serverAddress: string, userEmail: string } = {
     serverAddress: "http://localhost",
-    userEmail: "joey66666@aliyun.com"
+    userEmail: ""
 };
